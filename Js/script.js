40947cd58340ce3ea8381ecd2cf7978cb84e0123
Ulli-m <u.mentori@hotmail.com>
2024-05-18 21:19:37 +0200

    ulli

--- Js/script.js
+++ Js/script.js
@@ -1,7 +1,40 @@
 
 
 
-
+function simpleFetch(url){
+    
+    fetch("https://randomuser.me/api/")
+    .then(response => {
+    
+    if (!response.ok) {
+    throw new Error(`Något gick fel-` + response.status);
+    
+    }
+    
+    return response.json();
+    
+    })
+    
+    .then(data => {
+    console.log('Data hämtad:', data);
+    let joketext = data["setup"];
+    document.getElementById("mytext").innerText=joketext;
+    
+    
+    return data;
+    })
+    
+    .catch(error => {
+    console.error('Fetch-fel:', error);
+    
+    });
+    }
+    
+    function showData3(){
+        let joketext4 = document.getElementById("text3").value;          //id
+        let apiResponse = simpleFetch(joketext4);
+      
+     }   
 
 
 
